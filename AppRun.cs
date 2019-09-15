ddbbf66ad2fe04e2c48fa17d76c9cfab804354b3
NanYe <55266315+GIS-Dennis@users.noreply.github.com>
2019-09-14 21:52:01 -0700

    Add files via upload

---
+++ AppRun.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace HospitalDataAnalysis
+{
+    class AppRun
+    {
+        public static void Main()
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            HospitalDataProcessor processor1 = new HospitalDataProcessor(@"/Users/dennis/Desktop/cSharp/Hospital_General_Information.txt");
+
+            processor1.HospitalByStateGovernmentMeaningfulUseEHR("TX");
+            processor1.displayAndWriteSearchResults(@"/Users/dennis/Desktop/cSharp/new.txt");
+
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.ElapsedMilliseconds);
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
